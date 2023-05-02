056a6bd81c0d6abb01a94c5ad71debac5e2fa1cd
Adenation <Adenation>
2023-05-02 19:38:01 +0000

    Created Gold Tables
    - Also began writing queries

--- SchemaCreation.py.py
+++ SchemaCreation.py.py
@@ -135,43 +135,6 @@ for j in range(4):
 
 # COMMAND ----------
 
-
-
-# COMMAND ----------
-
-gold_tables = []
-# Loop through each table and convert it into star schema
-for table_name in table_list:
-    # Establish File Name
-    file_name = "/user/hive/warehouse/aden_bronze_database.db/aden_silver_{}".format(table_name[0])
-    # Read the Delta table
-    table_data = spark.read.format("delta").load(file_name)
-
-    # Add column names to the table
-    for i in range(len(schema_list[table_name[2]])):
-        table_data = table_data.withColumnRenamed(f"{i}", schema_list[table_name[2]][i][0].name) 
-
-    gold_tables.append(table_data)
-
-# COMMAND ----------
-
-gold_tables[2].show(1000)
-
-# COMMAND ----------
-
-payment_fact = gold_tables[0].join(gold_tables[1], "rider_id")
-payment_fact.show(10)
-
-# COMMAND ----------
-
-
-trip_fact = gold_tables[2].join(gold_tables[1], "rider_id").join(gold_tables[3], "start_station_id").join(station_dim, "end_station_id")
-
-for table in gold_tables:
-    table.write.format("delta").mode("overwrite").saveAsTable("aden_gold_" + table_list[i][0])
-
-# COMMAND ----------
-
 file_path = "/user/hive/warehouse/aden_bronze_database.db/aden_silver_"
 # 
 payments_file_path = file_path + table_list[0][0]
@@ -205,31 +168,199 @@ station_df.show(10)
 
 # COMMAND ----------
 
-# Create Fact Table 
-trip_fact_df = payment_df.join(rider_df, "rider_id")
+# Create Dimension Tables
+
+# Create Rideable_Type Dimension 
+rideable_dimension = trip_df.select("rideable_type").distinct() \
+    .withColumn("rideable_type_id", monotonically_increasing_id())
+
+rideable_dimension.show(10)
+# Create Payments Dimension
+payments_dimension = payment_df.select("payment_id", "date", "amount")
+
+payments_dimension.show(10)
+# Rider Dimension is the same as riders table
+
+# Create Trip_Date Dimension
+trip_date_dimension = trip_df.select("trip_id", "started_at", "ended_at").withColumn('duration in seconds', unix_timestamp(col('ended_at'))- unix_timestamp(col('started_at')))
+
+trip_date_dimension.show(10)
+# Station Dimension is the same as station table
+
+# COMMAND ----------
+
+# Create Fact Tables
+
+# Join Trip Dimension to new Rideable Dimension
+trip_df_2 = trip_df.join(rideable_dimension, "rideable_type")
+# Create Trip Fact Table
+trip_fact = trip_df_2.select("trip_id", "rider_id", "rideable_type_id", "start_station_id", "end_station_id")
 
 trip_fact.show(10)
+# Create Transaction Fact Table
+transaction_fact = payment_df.select("payment_id", "rider_id")
+
+transaction_fact.show(10)
+
+# COMMAND ----------
+
+# Write all of the Dataframes to Gold Tables
+
+file_path = "aden_gold_"
+transaction_fact.write.format("delta").mode("overwrite").option("overwriteSchema", True).saveAsTable(file_path + "fact_transaction")
+trip_fact.write.format("delta").mode("overwrite").option("overwriteSchema", True).saveAsTable(file_path + "fact_trip")
+payments_dimension.write.format("delta").mode("overwrite").option("overwriteSchema", True).saveAsTable(file_path + "dim_payment")
+rider_df.write.format("delta").mode("overwrite").option("overwriteSchema", True).saveAsTable(file_path + "dim_rider")
+station_df.write.format("delta").mode("overwrite").option("overwriteSchema", True).saveAsTable(file_path + "dim_station")
+rideable_dimension.write.format("delta").mode("overwrite").option("overwriteSchema", True).saveAsTable(file_path + "dim_rideable")
+trip_date_dimension.write.format("delta").mode("overwrite").option("overwriteSchema", True).saveAsTable(file_path + "dim_trip_time")
+
+# COMMAND ----------
+
+# Query 1) Time Spent per Ride based on Date and Time Factors
+query1 = trip_date_dimension.groupBy(date_format(col('started_at'), 'yyyy-MM-dd HH').alias('date_hour')) \
+    .agg(sum(col('duration in seconds')).alias('duration'))
+
+query1 = query1.withColumn('date_hour_ts', from_unixtime(unix_timestamp(col('date_hour'), 'yyyy-MM-dd HH'))) \
+  .withColumn('date', date_format(col('date_hour_ts'), 'yyyy-MM-dd')) \
+  .withColumn('hour', date_format(col('date_hour_ts'), 'HH'))
+
+query2 = trip_date_dimension.groupBy(date_format(col('started_at'), 'HH').alias('hour')) \
+    .agg(sum(col('duration in seconds')).alias('duration')) \
+    .sort(col('hour'))    
+
+query2.show()
+
+query3 = trip_date_dimension.withColumn("day_of_week", date_format(col('started_at'), "EEEE")) \
+    .groupBy(col("day_of_week").alias('day')) \
+    .agg(sum(col('duration in seconds')).alias('duration')) \
+    .sort(col('duration'))
+
+query3.show()
+
+# COMMAND ----------
+
+# Query time spent riding based on station
+
+query_start_station = trip_fact.join(trip_date_dimension, "trip_id") \
+    .withColumnRenamed('start_station_id', 'station_id') \
+    .join(station_df, "station_id")
+
+query4 = query_start_station.groupBy('station_id') \
+    .agg(sum(col('duration in seconds')).alias('duration')) \
+    .sort(desc(col('duration')))
+
+query4.show(10)
+
+query_end_station = trip_fact.join(trip_date_dimension, "trip_id") \
+    .withColumnRenamed('end_station_id', 'station_id') \
+    .join(station_df, "station_id")
+
+query5 = query_end_station.groupBy('station_id') \
+    .agg(sum(col('duration in seconds')).alias('duration')) \
+    .sort(desc(col('duration')))
+
+query5.show(10)
+
+
+query_both_station = trip_fact.join(trip_date_dimension, "trip_id") \
+    .withColumnRenamed('start_station_id', 'station_id') \
+    .join(station_df, "station_id") \
+    .withColumnRenamed('station_id', 'start') \
+    .withColumnRenamed('end_station_id', 'station_id') \
+    .join(station_df, "station_id") \
+
+query6 = query_both_station.groupBy('start', 'station_id') \
+    .agg(sum(col('duration in seconds')).alias('duration')) \
+    .sort(desc(col('duration')))
+
+query6.show(10)
+
+# COMMAND ----------
+
+# Query time spent riding based on age of rider
+
+query_time_age = trip_fact.join(trip_date_dimension, "trip_id") \
+    .join(rider_df, "rider_id") \
+    .withColumn("age", floor(datediff(current_date(), col("birthday")) / 365.25))
+
+query7 = query_time_age.groupBy(col("age")) \
+    .agg(sum(col('duration in seconds')).alias('duration')) \
+    .sort(desc(col('duration')))
+
+query7.show(10)
+
+query8 = query_time_age.groupBy(col("is_member")) \
+    .agg(sum(col('duration in seconds')).alias('duration')) \
+    .sort(desc(col('duration')))
+
+query8.show(10)
+
+# COMMAND ----------
+
+# Analyse how much money is spent
+#   ● Per month, quarter, year
+
+query_money_month = payments_dimension.groupBy(date_format(col('date'), 'MM').alias('month')) \
+    .agg(sum(col('amount')).alias('money')) \
+    .sort(asc(col('month')))
+
+query_money_month.show(12)
+
+query_money_year = payments_dimension.groupBy(date_format(col('date'), 'yyyy').alias('year')) \
+    .agg(sum(col('amount')).alias('money')) \
+    .sort(asc(col('year')))
+
+query_money_year.show(10)
+
+query_money_quarter = payments_dimension.groupBy(quarter('date').alias('quarter')) \
+    .agg(sum(col('amount')).alias('money')) \
+    .sort(asc(col('quarter')))
+
+query_money_quarter.show(10)
+
+# COMMAND ----------
+
+# Analyse how much money is spent
+#   ● Per member, based on the age of the rider at account start
+
+query_money_member = transaction_fact.join(payments_dimension, 'payment_id') \
+    .join(rider_df, 'rider_id') \
+    .filter(col("is_member") == True) \
+    .withColumn('age at joining', floor(datediff(col("account_start_date"), col("birthday")) / 365.25)) \
+    .groupBy(col('age at joining')) \
+    .agg(sum(col('amount')).alias('money spent')) \
+    .sort(desc(col('money spent')))
+query_money_member.show(10)
+
+# COMMAND ----------
+
+# Query
+# EXTRA CREDIT - Analyze how much money is spent per member
+#   ● Based on how many rides the rider averages per month
+#   ● Based on how many minutes the rider spends on a bike per month
+
+#read query plz
+query_total_spend_per_member = transaction_fact.join(payments_dimension, 'payment_id') \
+    .join(rider_df, 'rider_id') \
+    .filter(col("is_member") == True) \
+    .groupBy("rider_id") \
+    .agg(sum(col('amount')))
+
+query_rides_average_per_month = trip_fact.join(rider_df, 'rider_id') \
+    .filter(col("is_member") == True) \
+    .groupBy("rider_id").count() \
+    .withColumn('months since joining', floor(datediff(current_date(), col("account_start_date")) / 12))
+
+query_money_member.show(10)
+query_rides_average_per_month.show(10)
+
+# COMMAND ----------
+
+tf = trip_fact.groupBy(col("rider_id")).count().sort(desc(col("count")))
+
+tf.show(100)
 
 # COMMAND ----------
 
 
-
-# Create dimension tables
-rider_dim = rider_df.select(col("rider_id"), col("first_name"), col("last_name"), col("address"), col("birthday"), col("account_start_date"), col("account_end_date"), col("is_member"))
-station_dim = station_df.select(col("station_id"), col("name"), col("latitude"), col("longitude"))
-
-# Create fact tables
-payment_fact = payment_df.join(rider_dim, "rider_id")
-trip_fact = trip_df.join(rider_dim, "rider_id").join(station_dim, "start_station_id").join(station_dim, "end_station_id")
-
-# Write dimension tables to Delta
-rider_dim.write.format("delta").mode("overwrite").save("/path/to/rider_dim")
-station_dim.write.format("delta").mode("overwrite").save("/path/to/station_dim")
-
-# Write fact tables to Delta
-payment_fact.write.format("delta").mode("overwrite").save("/path/to/payment_fact")
-trip_fact.write.format("delta").mode("overwrite").save("/path/to/trip_fact")
-
-# COMMAND ----------
-
-# Add column names to tale
